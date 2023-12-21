0a18765b7142d5f51d053f487747194b57a52391
Yunkon Kim <hermitkim1@gmail.com>
2023-12-21 13:08:09 +0900

    Check and update the license notice in source files

--- cmd/import.go
+++ cmd/import.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 cychoi, tykim <dev@zconverter.com>
+Copyright 2023 The Cloud-Barista Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
