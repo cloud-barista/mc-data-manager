81d963dd306e094296661d9a6ee09027550b8685
heedaeshin <heedae.shin@namutech.co.kr>
2024-08-22 15:03:38 +0900

    update : swwagger docs  gcp param update

--- websrc/controllers/migrationS3Handlers.go
+++ websrc/controllers/migrationS3Handlers.go
@@ -141,6 +141,7 @@ func MigrationS3ToWindowsPostHandler(ctx echo.Context) error {
 // @Accept json
 // @Produce json
 // @Param RequestBody body MigrationForm true "Parameters required for migration"
+// @Param gcpCredential	formData file true "Parameters required to generate test data"
 // @Success 200 {object} models.BasicResponse "Successfully migrated data"
 // @Failure 500 {object} models.BasicResponse "Internal Server Error"
 // @Router /s3/gcp [post]
