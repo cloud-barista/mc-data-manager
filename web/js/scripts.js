d34efb40bbd98c400dda6592a026c2bba3387150
seungjae.oh <seungjae.oh@namutech.co.kr>
2025-10-15 08:30:31 +0000

    fix: fixed bug

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -486,8 +486,8 @@ function migrationFormSubmit() {
             alert("target credential not selected");
             return
         }
-        if(jsonData.targetPoint.bucket == "none" || jsonData.targetPoint.bucket == "-"
-            || jsonData.sourcePoint.bucket == "none" || jsonData.sourcePoint.bucket == "-"
+        if(jsonData.targetPoint.bucket == "none" || jsonData.targetPoint.bucket == "-" || jsonData.targetPoint.bucket == ""
+            || jsonData.sourcePoint.bucket == "none" || jsonData.sourcePoint.bucket == "-" || jsonData.sourcePoint.bucket == ""
         ) {
             alert("please select or create bucket");
             return
